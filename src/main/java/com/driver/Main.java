273f03c76c72ca31d29ba683b331fa3400cade2a
Shubham Chhangani <shubhamchhangani1998@gmail.com>
2023-02-09 09:13:27 +0530

    hi this is my first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Shubham");
+        String name = rwOnly.getName();
+        System.out.println(name);
+    }
 }
